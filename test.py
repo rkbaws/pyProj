0544197ad0f855806ba52ead5add920d7938d0cb
EC2 Default User <ec2-user@ip-10-0-1-6.ec2.internal>
2019-12-18 20:16:24 +0000

    3rd commit

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 import re
 ptrn=r'[A-Za-z0-9_.-]+@[A-Za-z0-9]+\.(com|net)'
+ptrn1=r'[A-Za-z0-9_.-]+@[A-Za-z0-9]+\.'
+
+
 string_to_search='''
 gygvhuqei
 random.random@ram.com
@@ -12,6 +15,12 @@ drtcyftgbyhnjmk@dbc.net
 ptrn_obj=re.compile(ptrn)
 
 matching_ptrn=ptrn_obj.finditer(string_to_search)
+match_all=re.findall(ptrn1, string_to_search)
+
+srch=ptrn_obj.search(string_to_search)
+print("if search is found:", srch)
+
+print(match_all)
 
 for each_match in matching_ptrn:
     print(each_match)
